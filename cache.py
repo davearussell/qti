742fe7a18ee99e31e5646687f2d9f44a449c48d1
David Russell <dave.a.russell@gmail.com>
2022-09-29 11:48:55 +0100

    improve reporting of cache progress

--- cache.py
+++ cache.py
@@ -16,30 +16,34 @@ class BackgroundCacher(Dispatcher):
     poll_interval_ms = 100
     worker_type = ImageCacher
 
-    def __init__(self, app, jobs, status_cb):
+    def __init__(self, app, jobs, skipped):
         super().__init__(app, jobs)
-        self.status_cb = status_cb
+        self.skipped = skipped
 
     def poll(self):
         super().poll()
+        text = "Cached %d / %d images" % (self.done + self.skipped, self.total + self.skipped)
         if self.done < self.total:
-            self.status_cb("Cached %d / %d images" % (self.done, self.total))
+            self.app.status_bar.set_text(text, priority=-10, duration_s=self.poll_interval_ms / 800)
         else:
-            self.status_cb('')
+            self.app.status_bar.set_text(text, duration_s=5)
+            self.stop()
 
 
-def background_cacher(app, root_dir, images, sizes, status_cb):
+def background_cacher(app, root_dir, images, sizes):
     jobs = []
+    skipped = 0
     for image in images:
         image_path = os.path.join(root_dir, image['path'])
         for size in sizes:
             cache_path = get_cached_path(root_dir, image_path, size)
             if os.path.exists(cache_path):
+                skipped += 1
                 continue
             if not os.path.isdir(os.path.dirname(cache_path)):
                 os.makedirs(os.path.dirname(cache_path))
             jobs.append((image_path, cache_path, size))
-    return BackgroundCacher(app, jobs, status_cb)
+    return BackgroundCacher(app, jobs, skipped) if jobs else None
 
 
 def get_cached_path(root_dir, image_path, size):
