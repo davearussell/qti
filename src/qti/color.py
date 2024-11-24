b27a04f4f01db21aac6438ff73e9186cb7bfe2e0
David Russell <dave.a.russell@gmail.com>
2024-11-24 09:23:09 +0000

    Color: disallow short-form hexcodes; pygame doesn't like them

--- src/qti/color.py
+++ src/qti/color.py
@@ -3,11 +3,13 @@ from colour import Color as CColor
 def to_rgb(name):
     return [round(v * 255) for v in CColor(name).rgb]
 
-
 # We inherit from str for convenience: it allows us to pass Color objects directly
 # into most backend APIs without first translating into a backend-specific object
 class Color(str):
     def __init__(self, name):
+        if len(name) == 4 and name.startswith('#'):
+            # Not all APIs accept these (in particular pygame does not)
+            raise ValueError("short-form hexcodes not accepted")
         self.name = str(name)
         self.rgb = to_rgb(self.name)
         self.r, self.g, self.b = self.rgb
